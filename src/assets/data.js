346a798392e2af3fd7fab81b0dd9942a9630eb90
Parahat <nepesovparahatt@gmail.com>
2021-05-07 14:07:56 +0300

    Added static JSON data

---
+++ src/assets/data.js
@@ -0,0 +1,371 @@
+const data_shop =  [
+            {
+                "id": "1",
+                "brand": "Apple",
+                "name": "iPhone 5",
+                "Memory": "16 Gb",
+                "ram": "2 Gb",
+                "price": "1000.00",
+                "img": "/images/iphone5.jpg",
+                "date": "2016-10-27T00:17:15.474Z",
+                "color": "blue"
+              },
+              {
+                "id": "2",
+                "brand": "Apple",
+                "name": "iPhone 6",
+                "Memory": "32 Gb",
+                "ram": "6 Gb",
+                "price": "1400.00",
+                "img": "/images/iphone6.jpg",
+                "date": "2018-01-05T19:52:46.267Z",
+                "color": "grey"
+              },
+              {
+                "id": "3",
+                "brand": "Apple",
+                "name": "iPhone 7",
+                "Memory": "64 Gb",
+                "ram": "4 Gb",
+                "price": "3500.00",
+                "img": "/images/iphone7.jpg",
+                "date": "2019-08-15T15:28:37.362Z",
+                "color": "pink"
+              },
+              {
+                "id": "4",
+                "brand": "Apple",
+                "name": "iPhone 7 Plus",
+                "Memory": "64 Gb",
+                "ram": "6 Gb",
+                "price": "1500.00",
+                "img": "/images/iphone7plus.jpg",
+                "date": "2019-03-14T13:30:49.325Z",
+                "color": "white"
+              },
+              
+              {
+                "id": "5",
+                "brand": "Apple",
+                "name": "iPhone 8 Plus",
+                "Memory": "64 Gb",
+                "ram": "6 Gb",
+                "price": "4500.00",
+                "img": "/images/iphone8.jpg",
+                "date": "2019-04-20T04:40:29.346Z",
+                "color": "dark blue"
+              },
+              {
+                "id": "6",
+                "brand": "Apple",
+                "name": "iPhone X",
+                "Memory": "64 Gb",
+                "ram": "3 Gb",
+                "price": "8500.00",
+                "img": "/images/iphoneX.jpg",
+                "date": "2019-03-14T12:10:49.106Z",
+                "color": "silver"
+              },
+              {
+                "id": "7",
+                "brand": "Apple",
+                "name": "iPhone 11",
+                "Memory": "64Gb",
+                "ram": "4 Gb",
+                "price": "11000.00",
+                "img": "/images/iphone11pro.jpg",
+                "date": "2020-04-10T09:30:40.776Z",
+                "color": "black"
+              },
+              {
+                "id": "8",
+                "brand": "Apple",
+                "name": "iPhone 12 Pro",
+                "Memory": "128 Gb",
+                "ram": "4 Gb",
+                "price": "13000.00",
+                "img": "/images/iphone12pro.jpg",
+                "date": "2020-07-16T18:21:28.862Z",
+                "color": "white"
+              },
+              {
+                "id": "9",
+                "brand": "Samsung",
+                "name": "Galaxy s3",
+                "Memory": "2 Gb",
+                "ram": "1 Gb",
+                "price": "840.00",
+                "img": "/images/samsung.jpg",
+                "date": "2012-03-06T06:57:45.237Z",
+                "color": "pink"
+              },
+              {
+                "id": "10",
+                "brand": "Samsung",
+                "name": "Galaxy s4",
+                "Memory": "12 Gb",
+                "ram": "2 Gb",
+                "price": "1040.00",
+                 "img": "/images/samsung1.jpg",
+                "date": "2017-03-18T17:06:40.380Z",
+                "color": "grey"
+              },
+              {
+                "id": "11",
+                "brand": "Samsung",
+                "name": "Galaxy s5",
+                "Memory": "32 Gb",
+                "ram": "2 Gb",
+                "price": "1200.00",
+                 "img": "/images/samsung2.jpg",
+                "date": "2018-11-02T03:37:16.695Z",
+                "color": "dark blue"
+              },
+              {
+                "id": "12",
+                "brand": "Samsung",
+                "name": "Galaxy s6",
+                "Memory": "28 Gb",
+                "ram": "3 Gb",
+                "price": "1840.00",
+                 "img": "/images/samsung3.jpg",
+                "date": "2018-02-26T07:38:12.995Z",
+                "color": "white"
+              },
+              {
+                "id": "13",
+                "brand": "Samsung",
+                "name": "Galaxy s7",
+                "Memory": "64 Gb",
+                "ram": " 4 Gb",
+                "price": "2540.00",
+                 "img": "/images/samsung4.jpg",
+                "date": "2018-07-16T00:38:12.621Z",
+                "color": "sky blue"
+              },
+              {
+                "id": "14",
+                "brand": "Samsung",
+                "name": "Galaxy s8",
+                "Memory": "64 Gb",
+                "ram": " 4 Gb",
+                "price": "2900.00",
+                 "img": "/images/samsung5.jpg",
+                "date": "2019-06-03T06:02:22.871Z",
+                "color": "yellow"
+              },
+              {
+                "id": "15",
+                "brand": "Samsung",
+                "name": "Galaxy s9",
+                "Memory": "128 Gb",
+                "ram": " 6 Gb",
+                "price": "3500.00",
+                 "img": "/images/samsung6.jpg",
+                "date": "2019-04-14T17:57:57.563Z",
+                "color": "red"
+              },
+              {
+                "id": "16",
+                "brand": "Samsung",
+                "name": "Galaxy s20",
+                "Memory": "128 Gb",
+                "ram": " 8 Gb",
+                "price": "12300.00",
+                 "img": "/images/samsung7.jpg",
+                "date": "2020-05-27T03:21:53.337Z",
+                "color": "blue"
+              },
+              {
+                "id": "17",
+                "brand": "Samsung",
+                "name": "A20",
+                "Memory": "32 Gb",
+                "ram": " 4 Gb",
+                "price": "3000.00",
+                 "img": "/images/samsung8.jpg",
+                "date": "2019-10-08T19:22:31.824Z",
+                "color": "grey"
+              },
+              {
+                "id": "18",
+                "brand": "Realme",
+                "name": "Realme Narzo 20 Pro",
+                "Memory": "128 Gb",
+                "ram": "64 Gb",
+                "price": "11146.00",
+                 "img": "/images/realme1.jpg",
+                "date": "2020-04-10T00:18:56.448Z",
+                "color": "black"
+              },
+              {
+                "id": "19",
+                "brand": "Realme",
+                "name": "Realme X2 Pro",
+                "Memory": "128 Gb",
+                "ram": "8 Gb",
+                "price": "3516.00",
+                 "img": "/images/realme2.jpg",
+                "date": "2020-03-21T20:56:53.640Z",
+                "color": "gold"
+              },
+              {
+                "id": "20",
+                "brand": "Realme",
+                "name": "Realme 6 Pro",
+                "Memory": "128 Gb",
+                "ram": "8 Gb",
+                "price": "3449.00",
+                 "img": "/images/realme3.jpg",
+                "date": "2020-09-17T01:50:11.928Z",
+                "color": "gold"
+              },
+              {
+                "id": "21",
+                "brand": "Realme",
+                "name": "Realme X2",
+                "Memory": "128 Gb",
+                "ram": "8 Gb",
+                "price": "2449.00",
+                 "img": "/images/realme4.jpg",
+                "date": "2020-04-26T19:08:06.071Z",
+                "color": "purple"
+              },
+              {
+                "id": "22",
+                "brand": "Realme",
+                "name": "Realme XT",
+                "Memory": "128 Gb",
+                "ram": "8 Gb",
+                "price": "3329.00",
+                 "img": "/images/realme5.jpg",
+                "date": "2020-05-01T22:23:12.100Z",
+                "color": "dark blue"
+              },
+              {
+                "id": "23",
+                "brand": "Realme",
+                "name": "Realme 5 Pro",
+                "Memory": "64 Gb",
+                "ram": "6 Gb",
+                "price": "2829.00",
+                 "img": "/images/realme6.jpg",
+                "date": "2019-04-09T12:48:57.649Z",
+                "color": "green"
+              },
+              {
+                "id": "24",
+                "brand": "Realme",
+                "name": "Realme 3 Pro",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "2629.00",
+                 "img": "/images/realme7.jpg",
+                "date": "2020-09-14T20:46:59.674Z",
+                "color": "gold"
+              },
+              {
+                "id": "25",
+                "brand": "Realme",
+                "name": "Realme 6i",
+                "Memory": "128 Gb",
+                "ram": "4 Gb",
+                "price": "2650.00",
+                 "img": "/images/realme8.jpg",
+                "date": "2019-12-31T14:27:05.726Z",
+                "color": "black"
+              },
+              {
+                "id": "26",
+                "brand": "Xiaomi",
+                "name": "Xiaomi Mi 10",
+                "Memory": "256 Gb",
+                "ram": "8 Gb",
+                "price": "8699.00",
+                "img": "/images/xiaomi1.jpg",
+                "date": "2020-04-27T10:17:32.084Z",
+                "color": "pink"
+              },
+              {
+                "id": "27",
+                "brand": "Xiaomi",
+                "name": "Poco F2 Pro",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "5649.00",
+                "img": "/images/xiaomi2.jpg",
+                "date": "2020-03-30T14:15:09.157Z",
+                "color": "black"
+              },
+              {
+                "id": "28",
+                "brand": "Xiaomi",
+                "name": "Mi 9T Pro",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "4649.00",
+                "img": "/images/xiaomi3.jpg",
+                "date": "2020-03-26T00:01:47.662Z",
+                "color": "blue"
+              },
+              {
+                "id": "29",
+                "brand": "Xiaomi",
+                "name": "Mi 9T",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "4229.00",
+                "img": "/images/xiaomi4.jpg",
+                "date": "2020-09-16T10:44:04.539Z",
+                "color": "red"
+              },
+              {
+                "id": "30",
+                "brand": "Xiaomi",
+                "name": "Mi 9T Pro",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "4649.00",
+                "img": "/images/xiaomi5.jpg",
+                "date": "2020-05-29T11:31:47.285Z",
+                "color": "gold"
+              },
+              {
+                "id": "31",
+                "brand": "Xiaomi",
+                "name": "Mi Note 10 Pro",
+                "Memory": "256 Gb",
+                "ram": "8 Gb",
+                "price": "7649.00",
+                "img": "/images/xiaomi6.jpg",
+                "date": "2020-10-16T18:28:31.717Z",
+                "color": "grey"
+              },
+              {
+                "id": "32",
+                "brand": "Xiaomi",
+                "name": "Mi 10 Lite",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "3999.00",
+                "img": "/images/xiaomi7.jpg",
+                "date": "2020-04-08T06:53:49.329Z",
+                "color": "red"
+              },
+              {
+                "id": "33",
+                "brand": "Xiaomi",
+                "name": "Redmi Note 9 Pro",
+                "Memory": "128 Gb",
+                "ram": "6 Gb",
+                "price": "2399.00",
+                "img": "/images/xiaomi8.jpg",
+                "date": "2019-11-26T20:09:40.005Z",
+                "color": "grey"
+              }
+        ]
+    
+
+export default data_shop;
+
+
